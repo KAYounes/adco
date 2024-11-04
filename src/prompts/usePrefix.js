007f7034c53aa61835c0d50c823c4fac4ed5da57
Kareem Amr Abdelsamie Abdelrahman Younes <67270781+KAYounes@users.noreply.github.com>
2024-11-04 07:36:03 +0200

    Update usePrefix.js

--- src/prompts/usePrefix.js
+++ src/prompts/usePrefix.js
@@ -1,43 +1,31 @@
 import { makeTheme, useEffect, useState } from '@inquirer/core';
-import { AsyncResource } from 'node:async_hooks';
 
 export function usePrefix({ status = 'idle', theme }) {
-  const [showLoader, setShowLoader] = useState(false);
+  const [showLoader, setShowLoader] = useState(status === 'loading');
   const [tick, setTick] = useState(0);
   const { prefix, spinner } = makeTheme(theme);
 
-  useEffect(
-    function () {
-      if (status === 'loading') {
-        let tickInterval;
-        let inc = -1;
+  useEffect(() => {
+    if (status === 'loading') {
+      let inc = -1;
+      setShowLoader(true);
 
-        // Show loader and start tick interval immediately
-        setShowLoader(true);
+      // Start tick interval immediately
+      const tickInterval = setInterval(() => {
+        inc = inc + 1;
+        setTick(inc % spinner.frames.length);
+      }, spinner.interval);
 
-        tickInterval = setInterval(
-          AsyncResource.bind(() => {
-            inc = inc + 1;
-            setTick(inc % spinner.frames.length);
-          }),
-          spinner.interval,
-        );
-
-        return () => {
-          clearInterval(tickInterval);
-        };
-      } else {
-        setShowLoader(false);
-      }
-    },
-    [status],
-  );
+      return () => clearInterval(tickInterval);
+    } else {
+      setShowLoader(false);
+    }
+  }, [status]);
 
   if (showLoader) {
     return spinner.frames[tick];
   }
 
-  // There's no delay anymore, so `loading` status shows the loader immediately.
   const iconName = status === 'loading' ? 'idle' : status;
   return typeof prefix === 'string' ? prefix : prefix[iconName] ?? prefix['idle'];
 }
