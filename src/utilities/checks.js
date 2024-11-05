a8723bfb3716af7fd97410af27178a67524b92b2
Kareem Amr Abdelsamie Abdelrahman Younes <67270781+KAYounes@users.noreply.github.com>
2024-11-05 08:00:28 +0200

    add checks to isValue in checks.js

--- src/utilities/checks.js
+++ src/utilities/checks.js
@@ -2,6 +2,15 @@ export function isFunction(query) {
   return typeof query === 'function';
 }
 
+// export function isValue(query) {
+//   return query !== undefined && query !== null && query !== '';
+// }
+
 export function isValue(query) {
-  return query !== undefined && query !== null && query !== '';
+  if (!query) return false;
+  if (query === undefined) return false;
+  if (query === null) return false;
+  if (query === '') return false;
+  if (Array.isArray(query) && query?.length === 0) return false;
+  return true;
 }
