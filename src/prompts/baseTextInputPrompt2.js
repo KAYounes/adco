81b4b4a6bf8e78057a169204ea852c992556bf64
Kareem Amr Abdelsamie Abdelrahman Younes <67270781+KAYounes@users.noreply.github.com>
2024-11-08 11:04:23 +0200

    Implement backspace debouncing

--- src/prompts/baseTextInputPrompt2.js
+++ src/prompts/baseTextInputPrompt2.js
@@ -1,9 +1,9 @@
-import { createPrompt, useState, useKeypress, isEnterKey, isBackspaceKey, makeTheme } from '@inquirer/core';
+import { createPrompt, useState, useKeypress, isEnterKey, isBackspaceKey, makeTheme, useRef } from '@inquirer/core';
 import figures from '@inquirer/figures';
 import chalk from 'chalk';
 import { usePrefix } from './usePrefix.js';
 import { getRawLength } from '#utilities/chalkUtils.js';
-import { _if, and, I } from '#utilities/util.js';
+import { _if, and, I, isEmpty } from '#utilities/util.js';
 import { handleEmptySpaces } from './common.js';
 
 import ansiEscapes from 'ansi-escapes';
@@ -16,16 +16,24 @@ const BaseTextInputPrompt = createPrompt((config, done) => {
   const prefix = usePrefix({ status, theme });
   const { required = false, validate = () => true, filter = I, transformer = I } = config;
 
+  const [c, setC] = useState(0);
+  const ignoreinput = useRef(false);
+  const ignoreTimeout = useRef();
+
   const [defaultInUse, setDefaultInUse] = useState(Boolean(config.default));
-  const defaultHintInit = chalk.gray(` {${config.default}}`);
+  const defaultHintInit = chalk.gray(
+    `Default [${chalk.yellow(config.default)}]: ${chalk.underline('remove (backspace)')} | ${chalk.underline(
+      'edit (tab)',
+    )}`,
+  );
   let [useDefault, setUseDefault] = useState(true);
-  const logs = [];
+  const [logs, setLogs] = useState([]);
 
   useKeypress(async (key, rl) => {
     if (isEnterKey(key)) {
       const answer = resolveAnswer(input, defaultInUse, config.default);
       const [valid, error] = isValidAnswer(answer, required, validate);
-      // console.log(`valid: ${valid}`);
+
       if (valid) {
         setInput(answer);
         setStatus('done');
@@ -39,22 +47,44 @@ const BaseTextInputPrompt = createPrompt((config, done) => {
         rl.write(input);
         setError(error);
       }
-    } else if (isBackspaceKey(key)) {
-      if (!input) {
-        if (!required) {
-          if (defaultInUse) {
-            setDefaultInUse(false);
-          }
-        } else {
-          setError('Default will be used since this answer is required.');
-        }
+    } else {
+      const currentInput = rl.line;
+
+      if (isEmpty(currentInput)) {
+        setDefaultInUse(true);
+      } else {
+        setDefaultInUse(false);
       }
 
-      setInput(rl.line); // read input
-      // setError(undefined); // clear errors
-    } else {
-      setInput(rl.line); // read input
-      setError(undefined); // clear errors
+      if (isBackspaceKey(key)) {
+        if (!ignoreinput.current) {
+          // why input and not currentInput? since we want to show the error message only if the user tries to delete the default value
+          if (isEmpty(input)) {
+            if (!required) setDefaultInUse(false);
+            else {
+              setError('Default is required since this answer is required.');
+            }
+          }
+        } else if (ignoreTimeout.current) {
+          clearTimeout(ignoreTimeout.current);
+        }
+
+        ignoreTimeout.current = setTimeout(function () {
+          ignoreinput.current = false;
+        }, 100);
+
+        ignoreinput.current = true;
+      } else if (key.name === 'tab') {
+        if (isEmpty(input)) {
+          rl.clearLine(0);
+          rl.write(config.default);
+          setInput(config.default);
+          setDefaultInUse(false);
+        }
+      } else {
+        setError(undefined); // clear errors
+      }
+      setInput(currentInput); // read input
     }
   });
 
@@ -65,8 +95,11 @@ const BaseTextInputPrompt = createPrompt((config, done) => {
     return chalk.green(`${prefix} ${message} |${formattedValue}|`);
   }
 
-  const defaultHint = _if(defaultInUse, ' ' + defaultHintInit, '');
-  return [`${prefix} ${message}${defaultHint} ${formattedValue}`, chalk.red.italic(errorMsg ?? '')];
+  const defaultHint = _if(defaultInUse, defaultHintInit, '');
+  return [
+    `${prefix} ${message} ${formattedValue}`,
+    [defaultHint, chalk.red.italic(errorMsg ?? ''), logs.slice(-10).join('\n')].filter(Boolean).join('\n'),
+  ];
 });
 
 export default BaseTextInputPrompt;
--- src/prompts/test/baseTextInputPrompTest.js
+++ src/prompts/test/baseTextInputPrompTest.js
@@ -4,7 +4,7 @@ import { handleEmptySpaces } from '#prompts/common.js';
 export async function startTextInputPromptTest() {
   const answer = await BaseTextInputPrompt({
     message: 'What is your name?',
-    required: true,
+    // required: true,
     default: 'John Smith',
     // filter: (x) => `you said: |${x}|`,
     validate: (input) => (input?.length > 3 ? true : 'Answer too short (4)'),
